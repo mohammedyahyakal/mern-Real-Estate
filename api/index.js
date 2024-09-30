f4312327dfb79702de82c0e7e22a083dc80a5bc8
mohammedyahyakal <98487400+mohammedyahyakal@users.noreply.github.com>
2024-09-30 22:28:23 +0300

    create sign in api route

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
-const app = express();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -13,12 +12,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const app = express();
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
 });
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
