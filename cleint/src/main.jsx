5b01314fc52540f4c69f233dde49ba863b15cafc
mohammedyahyakal <98487400+mohammedyahyakal@users.noreply.github.com>
2024-10-01 13:12:30 +0300

    create and add OAuth component

--- cleint/src/main.jsx
+++ cleint/src/main.jsx
@@ -12,3 +12,23 @@ createRoot(document.getElementById("root")).render(
     </PersistGate>
   </Provider>
 );
+
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDXe4ViXP7_2v-0DxNYXCNW484CHdXzlI",
+  authDomain: "real-estate-mern-58d99.firebaseapp.com",
+  projectId: "real-estate-mern-58d99",
+  storageBucket: "real-estate-mern-58d99.appspot.com",
+  messagingSenderId: "922396681742",
+  appId: "1:922396681742:web:70933b28284362d8a93e5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
